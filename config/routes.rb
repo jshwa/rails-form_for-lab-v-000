a5e700941c116d1662eeb5fef8600c33fc85b09e
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-12 23:57:17 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'students/new'
 
-  get 'students/create'
+  resourses :students, only: [:new, :create, :show, :edit, :update]
 
-  get 'students/show'
-
-  get 'students/edit'
-
-  get 'students/update'
 
   get 'school_class/new'
 
